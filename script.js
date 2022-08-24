3e18d635fe128b5e557074238d1db7ff38e3c60c
Shivam <shivamseth221104@gmail.com>
2022-08-24 20:10:59 +0530

    claculator

---
+++ script.js
@@ -0,0 +1,20 @@
+let result=document.getElementById("inputtext");
+
+let calculate=(number)=>{
+    result.value+=number;
+}
+
+let Result=()=>{
+    try{
+        result.value=eval(result.value);
+    }
+    catch(err){
+        alert("Enter Valid Input");
+    }
+}
+function clr(){
+    result.value=" ";
+}
+function  del(){
+    result.value=result.value.slice(0, -1) ;
+}
